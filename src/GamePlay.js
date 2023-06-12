834821d45c454e9f6886635a96cdf80c9c0deff8
vlady322 <120112740+vlady322@users.noreply.github.com>
2023-06-12 09:55:16 -0400

    GamePlay.js
    Signed-off-by: vlady322 <120112740+vlady322@users.noreply.github.com>

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,4 +1,3 @@
-
 GamePlayManager = {
     init: function() {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -10,54 +9,33 @@ GamePlayManager = {
     preload: function() {
         game.load.image('background', 'assets/images/background.png');
         //las demas imagenes
-        game.load.image('piedras', 'assets/images/piedras.png');     // 1 ********** 
-        game.load.image('ovni', 'assets/images/ovni.png');   // 1 ********** 
+        game.load.image('piedras', 'assets/images/piedras.png');     
+        game.load.image('ovni', 'assets/images/ovni.png');   
     },
     
     create: function() {
         game.add.sprite(0, 0, 'background');
-        this.shark = game.add.sprite(500,20,'piedras');          // 2 ********** 
-        this.fishes = game.add.sprite(100, 350, 'ovni');     // 2 ********** 
+        this.piedras = game.add.sprite(500,20,'piedras');         
+        this.ovni = game.add.sprite(100, 350, 'ovni');     
 
-        this.horse = game.add.sprite(0,0);
-        this.horse.frame = 0;
-        this.horse.anchor.setTo(0.5);
         game.input.onDown.add(this.onTap, this);
         
     },
 
         
-    onTap:function(){                      
-          this.flagFirstMouseDown = true;  
-    },                                     
     update: function() {
-        if(this.flagFirstMouseDown){ 
-            //para que las imagenes se muevan
-            this.shark.x--;           // 3 **********
-            if(this.shark.x<-300){    // 3 **********
-                this.shark.x = 1300;  // 3 **********
+        
+
+            this.piedras.x--;           
+            if(this.piedras.x<-300){    
+                this.piedras.x = 1300;  
             }
             
-            this.fishes.x+=0.3;        // 3 **********
-            if(this.fishes.x>1300){    // 3 **********
-                this.fishes.x = -300;  // 3 **********
-            }                         
-
-
-            var pointerX = game.input.x;          
-            var pointerY = game.input.y;           
+            this.ovni.x+=0.3;        
+            if(this.ovni.x>1300){    
+                this.ovni.x = -300;  
+            }                            
           
-            var distX = pointerX - this.horse.x;    
-            var distY = pointerY - this.horse.y;   
-
-            if(distX>0){ 
-                this.horse.scale.setTo(1,1);           
-            }else{       
-                this.horse.scale.setTo(-1,1);          
-            }
-            this.horse.x += distX * 0.02;              
-            this.horse.y += distY * 0.02;            
-        }  
     }
 }
 var game = new Phaser.Game(1136, 640, Phaser.CANVAS);
